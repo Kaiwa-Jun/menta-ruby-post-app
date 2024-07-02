6a9d7587876e15d25698223446d186a82a5289e1
Kaiwa-Jun <kj.11235813213455@gmail.com>
2024-07-02 23:10:07 +0900

    不必要なファイルの生成を避ける設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,17 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+    end
   end
 end
